bcbd763fa5198ae4c03121b3b4a439a244b0deee
luowei <luv135@163.com>
2021-01-13 17:05:30 +0800

    bip39 多个地址

--- src/main/kotlin/io/luowei/epirus/WalletTest.kt
+++ src/main/kotlin/io/luowei/epirus/WalletTest.kt
@@ -22,7 +22,8 @@ class WalletTest {
 //            val dir = File("walletDir").apply { mkdir() }
 //            val wallet = WalletUtils.generateBip39Wallet("123456", dir)
 //            println("mem = ${wallet.mnemonic}")
-            WalletTest().initWeb3();
+//            WalletTest().initWeb3();
+            WalletTest().createWallet();
         }
     }
 
@@ -36,18 +37,23 @@ class WalletTest {
 //
 //        val wallet = Bip44WalletUtils.generateBip44Wallet("123456", File("Bip44_wallet"))
 //        Bip32ECKeyPair.deriveKeyPair()
-        val seed = MnemonicUtils.generateSeed("anger winter either mutual spring achieve wet exhibit turkey enhance leader broom welcome valley note", null)
+        val seed = MnemonicUtils.generateSeed("dutch stem expand friend evidence appear ride plate eternal calm fabric swear", null)
         val masterKeyPair = Bip32ECKeyPair.generateKeyPair(seed)
-
-        // m/44'/60'/0'/0/0
-        val path = intArrayOf(44 or Bip32ECKeyPair.HARDENED_BIT, 60 or Bip32ECKeyPair.HARDENED_BIT, 0 or Bip32ECKeyPair.HARDENED_BIT, 0, 0)
-        val bip44KeyPair = Bip32ECKeyPair.deriveKeyPair(masterKeyPair, path)
-//        val bip44KeyPair = Bip44WalletUtils.generateBip44KeyPair(masterKeyPair, false)
-        val credentials = Credentials.create(bip44KeyPair)
-        println("address = ${credentials.address}")
+        for (i in 0..10)
+            chidAddress(masterKeyPair, i)
         //0x9052549e68d4315b44dc16fe02b560ce87d7062f
 //        0x1b5b507486620341dd178d9fd95a8c0887c1313b
 //        0x1B5B507486620341dD178D9FD95a8C0887c1313b
+    }
+
+    private fun chidAddress(masterKeyPair: Bip32ECKeyPair, index: Int) {
+        // m/44'/60'/0'/0/0
+        val path = intArrayOf(44 or Bip32ECKeyPair.HARDENED_BIT, 60 or Bip32ECKeyPair.HARDENED_BIT, 0 or Bip32ECKeyPair.HARDENED_BIT, 0, index)
+        val bip44KeyPair = Bip32ECKeyPair.deriveKeyPair(masterKeyPair, path)
+        //        val bip44KeyPair = Bip44WalletUtils.generateBip44KeyPair(masterKeyPair, false)
+        val credentials = Credentials.create(bip44KeyPair)
+
+        println("m/44'/60'/0'/0/$index Address = ${credentials.address} Private Key = 0x${credentials.ecKeyPair.privateKey.toString(16)}")
     }
 
     var rinkeby = "https://rinkeby.infura.io/v3/ac8fee58ba2744288c3c2199aec75684"
